8bc9ce51ab9b9f528951e93be4ae9a87431b713d
“Donica <donica.briggs@yahoo.com>
2023-06-22 20:07:32 +0000

    Module 1 Changes - Numerical Representations

---
+++ module1_input_function.py
@@ -0,0 +1,19 @@
+#print ('What is your name?')
+#user_name = input ()
+#print ('Hello,', user_name)
+
+#Another method is to include the input with the variable
+#user_name = input ('What is your name?')
+#print ('Hello,', user_name)
+
+#login = input ('Enter your login: ')
+#language = input ('Enter your native language: ')
+#print('Your login is', login, 'and you speak', language)
+
+print ('Enter your login: ')
+print ('Enter your native langauge: ')
+
+login_provided = input ()
+language_provided = input ()
+
+print ('Your login is,', login_provided, 'and you speak,', language_provided)
